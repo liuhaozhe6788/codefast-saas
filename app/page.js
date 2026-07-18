aecd95a3e1c758890e506c01dc82b8b8b45d9802
Haozhe Liu <liuhaozhe2000@gmail.com>
2026-07-18 16:30:57 +0200

    update hero section

--- app/page.js
+++ app/page.js
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <button className="btn">Get started</button>
+      <h1>Collect customer feedback to improve your product experience.</h1>
+      <div>
+        Create a feedback board in minutes, build products your customers love.
+      </div>
     </main>
   );
 }
